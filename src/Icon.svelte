c7b6cf4eb8a755198f31d8416605501c5c77091d
Fredy Sandoval <shockerovip@gmail.com>
2021-12-19 05:29:55 -0600

    fixing the icon issue

--- src/Icon.svelte
+++ src/Icon.svelte
@@ -1,6 +1,6 @@
 <script>
   import { afterUpdate } from "svelte";
-  export let name = 'light';
+  export let name = "light";
   export let width = "24px";
   export let height = "24px";
   export let focusable = false;
@@ -16,10 +16,14 @@
       svg: `<path fill="currentColor" d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"></path>`,
     },
   ];
-  let displayIcon = icons.find((e) => e.name === name);
+  // let displayIcon = icons.find((e) => e.name === name);
   afterUpdate(() => {
-    displayIcon = icons.find((e) => e.name === name);
+    let displayIcon = icons.find((e) => e.name === name);
+    let div = document.querySelectorAll('.darkmode-icon');
+    div.forEach(e => {
+      e.innerHTML = `<svg focusable="${focusable}" width="${width}" height="${height}" viewBox="0 0 ${displayIcon.box} ${displayIcon.box}">${displayIcon.svg}</svg>`
+    })
   });
 </script>
 
-<div>displayIcon</div>
+<div class="darkmode-icon"></div>
